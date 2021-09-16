c032d5c30eb4ed4d381b28b73e623fbb35e4b8ba
gabiuas <gabiuas1@Gmail.com>
2021-09-16 18:53:17 -0300

    Incrementing setup

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
-setup(name='dev_aberto_seunome',
+setup(name='dev_aberto_gabrieluas',
+      author="Gabriel Scodiero",
       version='0.1',
-      packages=['dev_aberto']
+      packages=['dev_aberto'],
+      python_requires=">=3.8",
+      platforms=["Windows", "Mac OS", "Linux"]
       )
