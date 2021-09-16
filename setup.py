94fc2840c2f58694daacef214b193b93355c93ff
gabiuas <gabiuas1@Gmail.com>
2021-09-16 18:57:35 -0300

    Adding requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(name='dev_aberto_gabrieluas',
       version='0.1',
       packages=['dev_aberto'],
       python_requires=">=3.8",
+      install_requires=["requests"],
       platforms=["Windows", "Mac OS", "Linux"]
       )
