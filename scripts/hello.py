e5bfb337816ea3503b2face077c5e42a72a2e5ed
gabiuas <gabiuas1@Gmail.com>
2021-09-21 21:23:48 -0300

    Locale Added

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from babel.dates import format_datetime
+import datetime
+import gettext
 
 if __name__ == '__main__':
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    date_locale = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
+
+    gettext.install('cli', localedir='locale') 
+    
+    print(_('Last commit made on:'), format_datetime(date_locale), _('by'), name)
